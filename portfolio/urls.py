1aa9fb1e708087b88cc7f68841cf37366d077e6a
Joseph <indiekaj@gmail.com>
2023-10-25 21:20:29 +0300

    Django Portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,5 @@ app_name = 'portfolio'
 urlpatterns = [
     path('',views.home, name='home'),
     path('about/',views.about, name='about'),
+    path('services/',views.services, name='services'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -8,4 +8,8 @@ def about(request):
     context = {'nav': 'about'}
     return render(request, 'about.html', context)
 
+def services(request):
+    context = {'nav': 'services'}
+    return render(request, 'services.html', context)
+
 # Create your views here.
